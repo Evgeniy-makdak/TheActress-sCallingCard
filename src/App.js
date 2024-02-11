e73db962806cbfd2cfc14ca333233fd386618ee6
Evgeniy <58253815+Evgeniy-makdak@users.noreply.github.com>
2024-02-11 21:21:01 +0300

    Add files via upload
    added button styles, added new components for site pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './styles/App.css';
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1 className="App_mainTitle">Виктория <br />Волкова</h1>
+      <div className="MyButton">
+        <MyButton>Биография</MyButton>
+        <MyButton>Портфолио</MyButton>
+        <MyButton>Резюме</MyButton>
+        <MyButton>Видео</MyButton>
+        <MyButton>Контакты</MyButton>
+      </div>
     </div>
   );
 }
